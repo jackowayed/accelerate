7697259836a95a3c82e44a1f08b7c237e2a5d0ea
Daniel Jackoway <danjdel@gmail.com>
2010-12-13 01:45:42 -0500

    generate base app

---
+++ src/com/danieljackoway/accelerate/AccelerateActivity.java
@@ -0,0 +1,10 @@
+package com.danieljackoway.accelerate;
+
+public class AccelerateActivity extends org.ruboto.RubotoActivity {
+	public void onCreate(android.os.Bundle arg0) {
+
+               setScriptName("accelerate_activity.rb");
+               super.onCreate(arg0);
+        }
+
+}
